fcee8131d5b79264862e0ef639bc574406c24212
Saif Maamun <sadifmamun@gmail.com>
2023-04-11 10:42:24 +0600

    getting all data

--- index.js
+++ index.js
@@ -16,11 +16,40 @@ app.use(cors());
 
 const uri = "mongodb+srv://CrudOperation:EBU7yQOrBHiOtGMx@cluster0.ogqtm.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+
+
+
+async function run() {
+    try {
+      const database = client.db("CrudOperation");
+      const usersdata = database.collection("user");
+      
+
+    //   get all data
+    app.get("/", async (req, res) => {
+        const query = {  };
+        const cursor = usersdata.find(query);
+        const results = await cursor.toArray()
+        res.send(results);
+
+    })
+    //   post a data
+    app.post("/user", async(req, res) => {
+        const user = req.body
+        const result = await usersdata.insertOne(user);
+        console.log(user)
+        res.send(result);
+    })
+      
+    } finally {
+    //   await client.close();
+    }
+  }
+  run().catch(console.dir);
+
+
+
+
 
 
 
